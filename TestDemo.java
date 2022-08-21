02fa9398ba5cd0acf2e7a7c27e7d8b9adbfd980c
Chaanyah Laborde <laborde.chaanyah@gmail.com>
2022-08-21 18:21:01 -0400

    New files added

---
+++ TestDemo.java
@@ -0,0 +1,14 @@
+public class TestDemo{
+	
+	public static void main(String[] args){
+		
+		for(int i = 1; i <= 20; i++){
+			
+			System.out.println("I am Chaanyah Laborde" + i);
+			
+		}
+		
+	}
+	
+}
+	
