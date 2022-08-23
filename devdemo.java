e8c7a89ce985263f8f482a0043d9d64a5054c9a3
Chaanyah Laborde <laborde.chaanyah@gmail.com>
2022-08-23 00:18:44 -0400

    New files added

---
+++ devdemo.java
@@ -0,0 +1,14 @@
+public class devdemo{
+	
+	public static void main(String[] args){
+		
+		for(int i = 1; i <= 20; i++){
+			
+			System.out.println("This is for developers" + i);
+			
+		}
+		
+	}
+	
+}
+	
