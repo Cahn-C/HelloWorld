69105f1d30c0d4a9990341d723d6fd8d8bb64fef
Chaanyah Laborde <laborde.chaanyah@gmail.com>
2022-09-28 19:33:22 -0400

    Change the repeating method to a new number.

--- devdemo.java
+++ devdemo.java
@@ -2,7 +2,7 @@ public class devdemo{
 	
 	public static void main(String[] args){
 		
-		for(int i = 1; i <= 20; i++){
+		for(int i = 1; i <= 30; i++){
 			
 			System.out.println("This is for developers" + i);
 			
